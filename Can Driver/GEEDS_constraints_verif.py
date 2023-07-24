f88b2698b369fe890d16978ea8c6f09e72e68c0e
youssef chaker <youssefchaker13@gmail.com>
2023-07-24 19:05:11 +0100

    work + logging

--- Can Driver/GEEDS_constraints_verif.py
+++ Can Driver/GEEDS_constraints_verif.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from tkinter import filedialog
 from lxml import etree
+import logging
+
+# Configure logging
+logging.basicConfig(filename='xdm_order_checker.log', level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 def ordered_by_RX_TX(xdm_file):
     try:
@@ -27,6 +31,8 @@ def ordered_by_RX_TX(xdm_file):
 
         if last_receive_frame_index is not None and first_transmit_frame_index is not None:
             if last_receive_frame_index > first_transmit_frame_index:
+                frame_name = ctr_elements[first_transmit_frame_index].attrib['name']
+                logging.error(f"Received frame '{frame_name}' comes after the first transmit frame.")
                 return False
 
             return True
@@ -35,7 +41,7 @@ def ordered_by_RX_TX(xdm_file):
             return False
 
     except Exception as e:
-        print(f"Error occurred while processing the XDM file: {e}")
+        logging.error(f"Error occurred while processing the XDM file: {e}")
         return False
 
 def ordered_by_CAN(xdm_file):
@@ -65,12 +71,14 @@ def ordered_by_CAN(xdm_file):
 
         for i in range(1, len(ordered_can_refs)):
             if ordered_can_refs[i - 1] > ordered_can_refs[i]:
+                frame_name = ctr_elements[i].attrib['name']
+                logging.error(f"The frame '{frame_name}' is not in the correct order.")
                 return False
 
         return True
 
     except Exception as e:
-        print(f"Error occurred while processing the XDM file: {e}")
+        logging.error(f"Error occurred while processing the XDM file: {e}")
         return False
 
 def ordered_by_id(xdm_file):
@@ -84,27 +92,42 @@ def ordered_by_id(xdm_file):
         ctr_elements = root.xpath(".//d:lst[@name='CanHardwareObject']/d:ctr", namespaces={'d': 'http://www.tresos.de/_projects/DataModel2/06/data.xsd'})
 
         # Extract the names of frames and their CanObjectId values in the order they appear in the file
-        frames_data = [(ctr, ctr.xpath("string(d:var[@name='CanObjectId']/@value)", namespaces={'d': 'http://www.tresos.de/_projects/DataModel2/06/data.xsd'})) for ctr in ctr_elements]
+        frames_data = [(ctr.attrib['name'], ctr.xpath("string(d:var[@name='CanObjectId']/@value)", namespaces={'d': 'http://www.tresos.de/_projects/DataModel2/06/data.xsd'})) for ctr in ctr_elements]
 
-        frames_data = [(ctr, obj_id) for ctr, obj_id in frames_data if obj_id.strip()]
+        frames_data = [(name, obj_id) for name, obj_id in frames_data if obj_id.strip()]
 
-        # Check if the CanObjectId values are unique
-        can_object_ids = [obj_id for _, obj_id in frames_data]
+        # Check if the first frame starts with CanObjectId 0
+        first_can_object_id = int(frames_data[0][1])
+        if first_can_object_id != 0:
+            logging.error(f"The first frame's CanObjectId should be '0', but found '{first_can_object_id}'.")
+            return False
+
+        # Check if the CanObjectId values are unique and in order
+        can_object_ids = [int(obj_id) for _, obj_id in frames_data]
         if len(can_object_ids) != len(set(can_object_ids)):
+            duplicates = [frame_name for frame_name, obj_id in frames_data if can_object_ids.count(int(obj_id)) > 1]
+            for frame_name in duplicates:
+                logging.error(f"The frame '{frame_name}' has a duplicate CanObjectId.")
             return False
 
         # Check if the frames are neatly ordered based on CanObjectId
+        last_can_object_id = int(frames_data[-1][1])
+        total_frames = len(frames_data)
+        if last_can_object_id != total_frames - 1:
+            logging.error(f"The last frame's CanObjectId should be '{total_frames - 1}', but found '{last_can_object_id}'.")
+            return False
+
         for i in range(1, len(frames_data)):
             if int(frames_data[i - 1][1]) > int(frames_data[i][1]):
+                frame_name = frames_data[i][0]
+                logging.error(f"The frame '{frame_name}' has a jump in CanObjectId.")
                 return False
 
         return True
 
     except Exception as e:
-        print(f"Error occurred while processing the XDM file: {e}")
+        logging.error(f"Error occurred while processing the XDM file: {e}")
         return False
-
-
 
 def browse_xml():
     xml_file_path = filedialog.askopenfilename(filetypes=[("XML files", "*XDM")])
@@ -122,6 +145,7 @@ def check_ordered_by_RX_TX():
 
     if is_ordered:
         result_label.config(text="The XDM file is ordered by RX-TX.", fg="green")
+        logging.info("The XDM file is ordered by RX-TX.")
     else:
         result_label.config(text="The XDM file is not ordered by RX-TX.", fg="red")
 
@@ -134,6 +158,7 @@ def check_ordered_by_CAN():
 
     if is_ordered:
         result_label.config(text="The XDM file is ordered by CAN.", fg="green")
+        logging.info("The XDM file is ordered by CAN.")
     else:
         result_label.config(text="The XDM file is not ordered by CAN.", fg="red")
 
@@ -146,8 +171,14 @@ def check_ordered_by_Id():
 
     if is_ordered:
         result_label.config(text="The XDM file is ordered by CanObjectId.", fg="green")
+        logging.info("The XDM file is ordered by CanObjectId.")
     else:
         result_label.config(text="The XDM file is not ordered by CanObjectId.", fg="red")
+
+def clear_log():
+    # Clear the log file
+    open('xdm_order_checker.log', 'w').close()
+    logging.info("Log file cleared.")
 
 root = tk.Tk()
 root.title("XML File Order Checker")
@@ -170,10 +201,13 @@ check_receive_transmit_button.grid(row=1, column=0, padx=5, pady=5)
 check_can_controller_ref_button = tk.Button(frame, text="Check Ordered by Can", command=check_ordered_by_CAN)
 check_can_controller_ref_button.grid(row=1, column=1, padx=5, pady=5)
 
-check_can_object_id_button = tk.Button(frame, text="Check Ordered by CanObjectId", command=check_ordered_by_Id)
+check_can_object_id_button = tk.Button(frame, text="Check Ordered by Id", command=check_ordered_by_Id)
 check_can_object_id_button.grid(row=1, column=2, padx=5, pady=5)
 
+clear_log_button = tk.Button(frame, text="Clear Log", command=clear_log)
+clear_log_button.grid(row=2, column=0, columnspan=3, padx=5, pady=5)  # Centered the button by spanning three columns
+
 result_label = tk.Label(frame, text="", font=("Arial", 12, "bold"))
-result_label.grid(row=2, column=0, columnspan=3, padx=5, pady=5)
+result_label.grid(row=3, column=0, columnspan=3, padx=5, pady=5)
 
 root.mainloop()
